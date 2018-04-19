3caf0ffba5ba018b39e43ee3bfae817cb8b62b00
Ella Fischenko <efischenko@cinimex.ru>
2018-04-19 17:26:47 +0300

    last Change for run application

--- core/src/main/java/ru/fella/Application.java
+++ core/src/main/java/ru/fella/Application.java
@@ -5,21 +5,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 import ru.fella.client.CursOnDateSevice;
+import ru.fella.dao.ValuteInfoDao;
+import ru.fella.entity.ValuteInfo;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner{
     @Autowired
     CursOnDateSevice cursOnDateSevice;
 
+    @Autowired
+    ValuteInfoDao valuteInfoDao;
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
         ValuteData vd = cursOnDateSevice.getValuteCurs();
         System.out.println(vd.getValuteCursOnDate().get(0).getVname());
+
+        ValuteInfo valuteInfo = new ValuteInfo();
+        valuteInfo.setValuteName("Австралийский доллар");
+        valuteInfoDao.save(valuteInfo);
     }
 }
